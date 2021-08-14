b5157fbd580c7b8840385a851c5cf3a9eda69cf9
Noire001 <noiredev@protonmail.com>
2021-08-14 22:14:22 +0300

    Add LinkProvider class: create control Link from UDP and requested Links from tcp

---
+++ app/src/main/java/com/kuromelabs/kurome/network/LinkProvider.kt
@@ -0,0 +1,34 @@
+package com.kuromelabs.kurome.network
+
+import com.kuromelabs.kurome.Packets
+import java.net.DatagramPacket
+import java.net.InetAddress
+import java.net.MulticastSocket
+
+@Suppress("BlockingMethodInNonBlockingContext")
+class LinkProvider {
+    suspend fun createControlLinkFromUdp(ip: String, port: Int): Link {
+        val socket = MulticastSocket(port)
+        val buffer = ByteArray(1024)
+        val group = InetAddress.getByName(ip)
+        socket.joinGroup(group)
+        val packet = DatagramPacket(buffer, buffer.size)
+        socket.receive(packet)
+        val msg = String(packet.data, packet.offset, packet.length)
+        socket.close()
+        val link = Link()
+        link.startConnection(msg.split(':')[1], 33588)
+        return link
+    }
+
+    suspend fun createLink(controlLink: Link): Link {
+        val message = controlLink.receiveMessage()
+        if (message[0] == Packets.ACTION_CREATE_NEW_LINK){
+            val link = Link()
+            link.startConnection(controlLink.ip, 33588)
+            return link
+        } else {
+            throw InstantiationException()
+        }
+    }
+}
