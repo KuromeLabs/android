c9bdd20a86815756645c12dadae56020992660c5
Noire001 <noiredev@protonmail.com>
2021-08-27 16:45:34 +0300

    Add permission dialog & fix for older Android versions

--- app/src/main/java/com/kuromelabs/kurome/UI/MainActivity.kt
+++ app/src/main/java/com/kuromelabs/kurome/UI/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.kuromelabs.kurome.UI
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.Settings
+import android.telecom.TelecomManager.DURATION_LONG
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.kuromelabs.kurome.BuildConfig
 import com.kuromelabs.kurome.R
+import java.lang.System.exit
+import kotlin.system.exitProcess
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-val fm = supportFragmentManager
+    val fm = supportFragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null) {
@@ -20,10 +29,6 @@ val fm = supportFragmentManager
                 .setReorderingAllowed(true)
                 .add(R.id.fragment_container_view, MainFragment::class.java, null)
                 .commit()
-        }
-        val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
-            startActivity(Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri))
         }
     }
 
--- app/src/main/java/com/kuromelabs/kurome/UI/MainFragment.kt
+++ app/src/main/java/com/kuromelabs/kurome/UI/MainFragment.kt
@@ -1,17 +1,29 @@
 package com.kuromelabs.kurome.UI
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.kuromelabs.kurome.BuildConfig
 import com.kuromelabs.kurome.KuromeApplication
 import com.kuromelabs.kurome.R
 import com.kuromelabs.kurome.database.DeviceRepository
@@ -26,11 +38,11 @@ import kotlinx.coroutines.launch
 import java.net.DatagramPacket
 import java.net.InetAddress
 import java.net.MulticastSocket
+import kotlin.system.exitProcess
 
-class MainFragment: Fragment(R.layout.fragment_main) {
-    var mContext: Context? = null
-    val pairingScope = CoroutineScope(Dispatchers.IO)
+class MainFragment : Fragment(R.layout.fragment_main) {
     private lateinit var repository: DeviceRepository
+    private var requestedManageFilePermissions = false
     private val deviceViewModel: DeviceViewModel by viewModels {
         DeviceViewModelFactory(repository)
     }
@@ -38,20 +50,15 @@ class MainFragment: Fragment(R.layout.fragment_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         repository = (activity?.application as KuromeApplication).repository
-        mContext = context
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mContext = context
-
-        val serviceIntent = Intent(context, ForegroundConnectionService::class.java)
         val startButton = view.findViewById<Button>(R.id.start_button)
         val stopButton = view.findViewById<Button>(R.id.stop_button)
-        startButton.setOnClickListener {
-            ContextCompat.startForegroundService(mContext!!, serviceIntent)
-        }
-        stopButton.setOnClickListener { mContext!!.stopService(serviceIntent) }
+        val serviceIntent = Intent(context, ForegroundConnectionService::class.java)
+        startButton.setOnClickListener(startServiceWithPermission)
+        stopButton.setOnClickListener { requireContext().stopService(serviceIntent) }
         val recyclerView: RecyclerView = view.findViewById(R.id.device_list)
         val deviceAdapter = DeviceAdapter {
             Log.e("kurome/mainfragment", "CLICKED! $it")
@@ -64,5 +71,82 @@ class MainFragment: Fragment(R.layout.fragment_main) {
         deviceViewModel.combinedDevices.observe(viewLifecycleOwner) { devices ->
             devices.let { deviceAdapter.submitList(it) }
         }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                val serviceIntent = Intent(context, ForegroundConnectionService::class.java)
+                requireContext().startService(serviceIntent)
+            } else {
+                Toast.makeText(
+                    context,
+                    "Kurome can't work without File Access permissions.",
+                    Toast.LENGTH_LONG
+                ).show()
+                exitProcess(0)
+            }
+        }
+
+
+    private val startServiceWithPermission: (View) -> Unit = {
+        val serviceIntent = Intent(context, ForegroundConnectionService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager())
+                getFilePermission(true)
+            else
+                requireContext().startService(serviceIntent)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            )
+                requireContext().startService(serviceIntent)
+            else
+                getFilePermission(false)
+        }
+    }
+
+    @SuppressLint("NewApi")
+    override fun onResume() {
+        super.onResume()
+        //If we are returning from Settings after requesting MANAGE_EXTERNAL_STORAGE, which is
+        //only after the user wants to start the service, check if we can start the service.
+        if (requestedManageFilePermissions && Environment.isExternalStorageManager()){
+            val serviceIntent = Intent(context, ForegroundConnectionService::class.java)
+            requireContext().startService(serviceIntent)
+            requestedManageFilePermissions = false
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun getFilePermission(isROrHigher: Boolean) {
+        val builder = AlertDialog.Builder(requireContext())
+            .setTitle("File Access Permission")
+            .setMessage(
+                "Kurome is a filesystem application and needs to manage your device's storage." +
+                        " Please allow access to all files in the next screen."
+            )
+            .setCancelable(true)
+            .setPositiveButton("OK") { _: DialogInterface, _: Int ->
+                if (isROrHigher) {
+                    requestedManageFilePermissions = true
+                    val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+                    startActivity(Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri))
+                } else
+                    requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+            .setNegativeButton("NO THANKS") { _: DialogInterface, _: Int ->
+                Toast.makeText(
+                    context,
+                    "Kurome can't work without File Access permissions.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        val alert = builder.create()
+        alert.show()
     }
 }
