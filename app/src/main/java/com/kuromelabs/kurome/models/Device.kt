86ede973dbf047c993ec9db44cb636fd8e254ec4
Noire001 <noiredev@protonmail.com>
2021-12-23 23:47:26 +0200

    Device: add methods to use FlatBuffers packets

--- app/src/main/java/com/kuromelabs/kurome/models/Device.kt
+++ app/src/main/java/com/kuromelabs/kurome/models/Device.kt
@@ -148,6 +148,171 @@ data class Device(
         }
     }
 
+    private fun parsePacket(packet: Packet, builder: FlatBufferBuilder): ByteBuffer {
+        var deviceInfo = 0
+        var fileNodes = IntArray(0)
+        var raw = 0
+        var result = Result.noResult
+        val path = root + packet.path
+        when (packet.action) {
+            Action.actionGetSpaceInfo -> deviceInfo = getSpaceInfo(builder)
+            Action.actionGetDirectory -> fileNodes = getDirectory(builder, path)
+            Action.actionCreateDirectory -> result = createDirectory(path)
+            Action.actionGetFileType -> result = getFileType(path)
+            Action.actionDelete -> result = delete(path)
+            Action.actionReadFileBuffer -> raw = readFileBuffer(builder, packet)
+            Action.actionGetFileInfo -> fileNodes = intArrayOf(getFileNode(builder, path))
+            Action.actionWriteFileBuffer -> result = writeFileBuffer(packet)
+            Action.actionRename -> result = rename(packet)
+            Action.actionSetFileTime -> result = setFileTime(packet)
+            Action.actionCreateFile -> result = createFile(path)
+        }
+        val nodesVector = Packet.createNodesVector(builder, fileNodes)
+        Packet.startPacket(builder)
+        Packet.addNodes(builder, nodesVector)
+        Packet.addResult(builder, result)
+        Packet.addDeviceInfo(builder, deviceInfo)
+        Packet.addFileBuffer(builder, raw)
+        val res = Packet.endPacket(builder)
+        builder.finishSizePrefixed(res)
+        return builder.dataBuffer()
+    }
+
+    private fun getSpaceInfo(builder: FlatBufferBuilder): Int {
+        DeviceInfo.startDeviceInfo(builder)
+        DeviceInfo.addFreeBytes(builder, StatFs(Environment.getDataDirectory().path).availableBytes)
+        DeviceInfo.addTotalBytes(builder, StatFs(Environment.getDataDirectory().path).totalBytes)
+        return DeviceInfo.endDeviceInfo(builder)
+    }
+
+    private fun getDirectory(builder: FlatBufferBuilder, path: String): IntArray {
+        val files = File(path).listFiles()
+        val nodes = IntArray(files!!.size)
+        files.forEachIndexed { i, file -> nodes[i] = getFileNode(builder, file.path) }
+        return nodes
+    }
+
+    private fun createDirectory(path: String): Byte {
+        return if (File(path).mkdirs()) Result.resultActionSuccess
+        else Result.resultActionFail
+    }
+
+    private fun getFileType(path: String): Byte {
+        val file = File(path)
+        return if (file.exists())
+            if (file.isDirectory) Result.resultFileIsDirectory
+            else Result.resultFileIsFile
+        else {
+            val directory = File(path.substring(0, path.lastIndexOf("/")))
+            if (!directory.exists()) Result.resultPathNotFound
+            else Result.resultFileNotFound
+        }
+    }
+
+    private fun delete(path: String): Byte {
+        return if (File(path).deleteRecursively()) Result.resultActionSuccess
+        else Result.resultActionFail
+    }
+
+    private fun readFileBuffer(builder: FlatBufferBuilder, packet: Packet): Int {
+        val length = packet.fileBuffer!!.length
+        val offset = packet.fileBuffer!!.offset
+        val path = root + packet.path
+        val fis = File(path).inputStream()
+        var count = 0
+        var pos = offset
+        val buffer = ByteArray(length)
+        while (count != length) {
+            Timber.d("reading file buffer ($length) at $pos of file $path")
+            fis.channel.position(pos)
+            count += fis.read(buffer, count, length - count)
+            pos += count
+        }
+        fis.close()
+        val byteVector = Raw.createRawVector(builder, buffer)
+        Raw.startRaw(builder)
+        Raw.addRaw(builder, byteVector)
+        return Raw.endRaw(builder)
+    }
+
+    private fun getFileNode(builder: FlatBufferBuilder, path: String): Int {
+        val file = File(path)
+        var crTime = 0L
+        val lwTime = file.lastModified()
+        var laTime = 0L
+        val filename = builder.createString(file.name)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val pathObj = Paths.get(path)
+            val attributes = Files.getFileAttributeView(pathObj, BasicFileAttributeView::class.java)
+            crTime = attributes.readAttributes().creationTime().toMillis()
+            laTime = attributes.readAttributes().lastAccessTime().toMillis()
+        }
+        return kurome.FileNode.createFileNode(
+            builder,
+            filename,
+            file.isDirectory,
+            file.length(),
+            crTime,
+            laTime,
+            lwTime
+        )
+    }
+
+    private fun writeFileBuffer(packet: Packet): Byte {
+        val raf = RandomAccessFile(root + packet.path, "rw")
+        val offset = packet.fileBuffer!!.offset
+        try {
+            val actualOffset = if (offset == (-1).toLong()) raf.length() else offset
+            raf.seek(actualOffset) //offset = -1 means append
+            Timber.d("writing file buffer at $offset")
+            raf.write(packet.fileBuffer!!.rawAsByteBuffer.array())
+            raf.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return Result.resultActionFail
+        }
+        return Result.resultActionSuccess
+    }
+
+    private fun rename(packet: Packet): Byte {
+        return try {
+            val oldPath = root + packet.path!!
+            val newPath = root + packet.nodes(0)!!.filename!!
+            File(oldPath).renameTo(File(newPath))
+            Result.resultActionSuccess
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Result.resultActionFail
+        }
+    }
+
+    private fun setFileTime(packet: Packet): Byte {
+        val path = root + packet.nodes(0)!!.filename!!
+        val crTime = packet.nodes(0)!!.creationTime
+        val laTime = packet.nodes(0)!!.lastAccessTime
+        val lwTime = packet.nodes(0)!!.lastWriteTime
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val attributes = Files.getFileAttributeView(
+                Paths.get(path),
+                BasicFileAttributeView::class.java
+            )
+            attributes.setTimes(
+                FileTime.fromMillis(crTime),
+                FileTime.fromMillis(laTime),
+                FileTime.fromMillis(lwTime)
+            )
+        } else { //
+            val file = File(path)
+            file.setLastModified(lwTime)
+        }
+        return Result.resultActionSuccess
+    }
+
+    private fun createFile(path: String): Byte {
+        return if (File(path).createNewFile()) Result.resultActionSuccess
+        else Result.resultActionFail
+    }
+
     private fun parseMessage(bytes: ByteArray): ByteArray {
         val message: String? = if (bytes.size > 1) String(bytes, 1, bytes.size - 1) else null
         when (bytes[0]) {
