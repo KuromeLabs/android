b64ab67f61953bb8c2f7238d5caa5f7449059e09
Noire001 <noiredev@protonmail.com>
2022-02-08 23:35:41 +0200

    Start KuromeService on boot

---
+++ app/src/main/java/com/kuromelabs/kurome/KuromeBroadcastReceiver.kt
@@ -0,0 +1,24 @@
+package com.kuromelabs.kurome
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import com.kuromelabs.kurome.services.KuromeService
+import timber.log.Timber
+
+class KuromeBroadcastReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val action = intent?.action
+        if (action == Intent.ACTION_BOOT_COMPLETED) {
+            Timber.d("KuromeBroadcastReceiver: Boot Completed")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context?.startForegroundService(Intent(context, KuromeService::class.java))
+            } else {
+                context?.startService(Intent(context, KuromeService::class.java))
+            }
+        }
+    }
+
+}
