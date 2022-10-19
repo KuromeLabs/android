b5f655ca647fbda0507fb00e567a0d2af0ac8c43
Konstantinos Karakostas <noiredev@protonmail.com>
2022-10-19 12:27:58 +0300

    gradle: update version & libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
 android {
     project.tasks.preBuild.get().dependsOn("generateFbsKotlin")
     project.tasks.preBuild.get().mustRunAfter("generateFbsKotlin")
-    compileSdk = 32
-    buildToolsVersion = "32.1.0-rc1"
+    compileSdk = 33
+//    buildToolsVersion = "32.1.0-rc1"
     useLibrary("android.test.base")
     useLibrary("android.test.mock")
     defaultConfig {
         applicationId = "com.kuromelabs.kurome"
         minSdk = 21
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -43,7 +43,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.2.0-rc02"
+        kotlinCompilerExtensionVersion = "1.3.2"
     }
 
     kotlinOptions {
@@ -53,6 +53,7 @@ android {
     sourceSets.getByName("main") {
         java.srcDir("build/generated/source/fsb")
     }
+    namespace = "com.kuromelabs.kurome"
 
     tasks.register<Exec>("generateFbsKotlin") {
         doFirst {
@@ -83,29 +84,29 @@ android {
 
 dependencies {
 
-    implementation("androidx.lifecycle:lifecycle-service:2.4.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-service:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
 
-    kapt("androidx.room:room-compiler:2.4.2")
-    implementation("com.google.accompanist:accompanist-permissions:0.24.10-beta")
+    kapt("androidx.room:room-compiler:2.4.3")
+    implementation("com.google.accompanist:accompanist-permissions:0.26.5-rc")
 
-    implementation("com.google.dagger:hilt-android:2.42")
-    kapt("com.google.dagger:hilt-android-compiler:2.42")
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
-    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("com.google.android.material:material:1.6.1")
-    implementation("androidx.room:room-runtime:2.4.2")
-    implementation("androidx.room:room-ktx:2.4.2")
+    implementation("androidx.room:room-runtime:2.4.3")
+    implementation("androidx.room:room-ktx:2.4.3")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("com.jakewharton.timber:timber:5.0.1")
-    implementation("com.google.flatbuffers:flatbuffers-java:2.0.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.2")
+    implementation("com.google.flatbuffers:flatbuffers-java:2.0.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:4.6.1")
+    testImplementation("org.mockito:mockito-core:4.8.1")
     testImplementation("androidx.test:core:1.4.0")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
@@ -115,25 +116,25 @@ dependencies {
     androidTestImplementation("androidx.test:rules:1.4.0")
 
     // Compose
-    implementation("androidx.compose.runtime:runtime:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.foundation:foundation:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.foundation:foundation-layout:${rootProject.extra["compose_version"]}")
-    implementation("androidx.navigation:navigation-compose:2.4.2")
+    implementation("androidx.compose.runtime:runtime:1.2.1")
+    implementation("androidx.compose.ui:ui:1.2.1")
+    implementation("androidx.compose.foundation:foundation:1.2.1")
+    implementation("androidx.compose.foundation:foundation-layout:1.2.1")
+    implementation("androidx.navigation:navigation-compose:2.5.2")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
-    implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.material:material:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling:1.2.1")
+    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.2.1")
 
     //leakcanary
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
 
     //SSL
-    implementation("org.bouncycastle:bcpkix-jdk18on:1.71")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+    implementation("org.bouncycastle:bcpkix-jdk18on:1.72")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.0")
 
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 buildscript {
-    val kotlinVersion = "1.6.21"
+    val kotlinVersion = "1.7.20"
     extra.apply {
-        set("compose_version", "1.2.0-rc02")
+        set("compose_version", "1.2.1")
     }
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.0-alpha05")
+        classpath("com.android.tools.build:gradle:8.0.0-alpha04")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
     }
